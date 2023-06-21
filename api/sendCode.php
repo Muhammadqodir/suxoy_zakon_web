ee3bf146d3b8e254037cde1b26e799ba446fa6cb
Muhammadqodir <m.qodir777@gmail.com>
2023-06-21 14:32:49 +0300

    Created snedCode endpoint

---
+++ api/sendCode.php
@@ -0,0 +1,58 @@
+<?php
+require "../db_helper.php";
+require "validators.php";
+require "config.php";
+
+// $token = "undefined";
+// try{
+//   if(isset(getallheaders()["Token"])){
+//     echo getallheaders()["Token"];
+//   }
+// }catch{
+
+// }
+
+$res = [];
+if (validateRequest()) {
+
+  $curl = curl_init();
+  $code = rand(1000, 9999);
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://new.smsgorod.ru/apiSms/create',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{
+      "apiKey": "'.$smsToken.'",
+      "sms": [
+        {
+          "channel": "char",
+          "phone": "'.str_replace("+", "", $_POST["phone"]).'",
+          "text": "Ваш код подтверждения для СухойЗаконЪ. Код: '.$code.'",
+          "sender": "VIRTA"
+        }
+      ]
+    }',
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json'
+    ),
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+  $res["isSuccess"] = true;
+  $res["code"] = $code."";
+  $res["message"] = $response;
+} else {
+  $res["isSuccess"] = false;
+  $res["message"] = "Bad request";
+}
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($res);
--- test.php
+++ test.php
@@ -1,10 +1,33 @@
 <?php
-// phpinfo();
-// require("db_helper.php");
 
-// $db = new DBHelper();
+$curl = curl_init();
 
-// $users = $db->getAllMenu();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://new.smsgorod.ru/apiSms/create',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+    "apiKey": "FMsE2bJ9glMERHL4LTLpXhezCIbkgrSqb58iKC0g8ps995WRUi9pNyCwKQIT",
+    "sms": [
+        {
+            "channel": "char",
+            "sender": "VIRTA",
+            "text": "test1",
+            "phone": "79682659013"
+        }
+    ]
+}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
 
-// print_r($users);
+$response = curl_exec($curl);
 
+curl_close($curl);
+echo $response;
