5af17bc7c013e10d21985aef2d97ab671717722a
Muhammadqodir <m.qodir777@gmail.com>
2023-06-22 04:18:58 +0300

    Improved sendCode endpoint for test user

--- api/sendCode.php
+++ api/sendCode.php
@@ -15,40 +15,45 @@ require "config.php";
 $res = [];
 if (validateRequest()) {
 
-  $curl = curl_init();
-  $code = rand(1000, 9999);
+  if ($_POST["phone"] == "+79998887766") {
+    $res["isSuccess"] = true;
+    $res["code"] = "1234";
+  } else {
+    $curl = curl_init();
+    $code = rand(1000, 9999);
 
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://new.smsgorod.ru/apiSms/create',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => '{
-      "apiKey": "'.$smsToken.'",
-      "sms": [
-        {
-          "channel": "char",
-          "phone": "'.str_replace("+", "", $_POST["phone"]).'",
-          "text": "Ваш код подтверждения для СухойЗаконЪ. Код: '.$code.'",
-          "sender": "VIRTA"
-        }
-      ]
-    }',
-    CURLOPT_HTTPHEADER => array(
-      'Content-Type: application/json'
-    ),
-  ));
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://new.smsgorod.ru/apiSms/create',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS => '{
+        "apiKey": "' . $smsToken . '",
+        "sms": [
+          {
+            "channel": "char",
+            "phone": "' . str_replace("+", "", $_POST["phone"]) . '",
+            "text": "Ваш код подтверждения для СухойЗаконЪ. Код: ' . $code . '",
+            "sender": "VIRTA"
+          }
+        ]
+      }',
+      CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+      ),
+    ));
 
-  $response = curl_exec($curl);
+    $response = curl_exec($curl);
 
-  curl_close($curl);
-  $res["isSuccess"] = true;
-  $res["code"] = $code."";
-  $res["message"] = $response;
+    curl_close($curl);
+    $res["isSuccess"] = true;
+    $res["code"] = $code . "";
+    $res["message"] = $response;
+  }
 } else {
   $res["isSuccess"] = false;
   $res["message"] = "Bad request";
